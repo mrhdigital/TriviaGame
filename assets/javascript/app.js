9c4e5aa738cb13fb74f72a4b8e7c5976abf6753b
unknown <mrhdigital@yahoo.com>
2018-04-11 16:07:22 -0500

    added loadquesion and related function and loaded images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -73,9 +73,9 @@ var game = {
       correct: 0,
       incorrect: 0,
       unanswered: 0,
-      questions: questions,
+      questions: 0,
       currentQuestion:0,
-      
+
       countdown: function() {
         game.counter--;
         //populate the decremental values of counter in html
@@ -95,17 +95,73 @@ var game = {
           $("#subwrapper").html("<h5> TIME REMAINING <span id = 'counter'> 30 </span> Seconds </h5>");
           // create the countdown time for each second
           timer = setInterval(game.countdown, 1000);
+          // select value of property question from the array questions of index value zero
+          // and append to html.
+          $("#subwrapper").append("<h2>" + questions[game.currentQuestion].
+            question + "</h2>");
+            //append all the answers of the question selected
+            var numOfTotalAnswers = questions[game.currentQuestion].answers.length;
+            for(var i = 0; i < numOfTotalAnswers; i++) {
+                //create the buttons and store the answers in the button using "data-name"
+                $('#subwrapper').append('<button class = "answer-button" id = "button-'+ 
+                i + '" data-name = "' +questions[game.
+                currentQuestion].answers[i]+'">' + questions[game.
+                currentQuestion].answers[i]+'</button>' );
+            }
       },
       nextQuestion: function() {
         game.counter = 30;
         $("#counter").html(game.counter);
-        
+        game.currentQuestion++;
         game.loadQuestion();
     },
     timeUp: function() {
         clearInterval(timer);
                     setTimeout(game.nextQuestion,3*1000);
     
+    },
+
+    clicked: function(e) {
+        clearInterval(timer);
+        if($(e.target).data("name")==questions[game.currentQuestion].correctAnswer) {
+            game.answeredCorrectly();                
+        }
+        else {
+            game.answeredIncorrectly();
+        }
+
+    },
+    answeredCorrectly: function() {
+        console.log("YOU GOT IT!");
+        clearInterval(timer);
+        game.correct++;
+        $("#subwrapper").html("<h2> YOU GOT IT RIGHT! </h2>");
+                
+
+        $("#subwrapper").append('<img src="' + questions[game.currentQuestion].image + '" />');
+        if(game.currentQuestion == questions.length-1) {
+            setTimeout(game.results, 3*1000);
+        }
+        else {
+            setTimeout(game.nextQuestion,3*1000);
+        }
+
+    },
+    answeredIncorrectly: function () {
+        console.log("WRONG !");
+        clearInterval(timer);
+        game.incorrect++;
+        console.log(game.incorrect);
+        $("#subwrapper").html("<h2 style = 'color:red;'' > YOU GOT IT WRONG! </h2>");
+        $("#subwrapper").append("<h3> The Correct Answer Was:"+
+        questions[game.currentQuestion].correctAnswer+ " </h3>");
+        $("#subwrapper").append('<img src="' + questions[game.currentQuestion].image + '" />');
+        if(game.currentQuestion == questions.length-1) {
+            setTimeout(game.results, 3*1000);
+        }
+        else {
+            setTimeout(game.nextQuestion,3*1000);
+        }
     },
 }
 
@@ -121,4 +177,8 @@ $("#start").on("click",function() {
     $("#start").remove();
     game.loadQuestion();
     
+})
+$(document).on("click", ".answer-button", function(e) {
+    game.clicked(e);
+
 })
